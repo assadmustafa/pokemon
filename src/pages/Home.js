d25e6f70b3c36dbdbefef67ad7c44d689a903fd3
Yigit Findik <yigit8@live.nl>
2023-12-01 12:15:31 +0100

    added mute button for theme song
    removed the audio player from home.js to landingpage.js so i could add better place for the mute button inside landingpage.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import LandingPage from "./LandingPage";
-import ReactAudioPlayer from 'react-audio-player';
+// import ReactAudioPlayer from 'react-audio-player';
 
 
 const Home = () => {
-  
+    // const [isMuted, setIsMuted] = useState(false);
+
+    // const handleToggleMute = () => {
+    //     setIsMuted(!isMuted);
+    // };
+
     return <div>
-      <LandingPage></LandingPage>
-      <ReactAudioPlayer
-        src="/sounds/PokémonThemeSong.mp3"
-        autoPlay
-      />
+        <LandingPage />
+        {/*<ReactAudioPlayer*/}
+        {/*    src="/sounds/PokémonThemeSong.mp3"*/}
+        {/*    autoPlay*/}
+        {/*    volume={isMuted ? 0 : 1} // Set volume to 0 when muted, 1 otherwise*/}
+        {/*/>*/}
+        {/*<button onClick={handleToggleMute}>*/}
+        {/*    {isMuted ? "Unmute" : "Mute"}*/}
+        {/*</button>*/}
     </div>
-  };
+};
   
 export default Home;
