f3fc2d63ffbc1a46d6c1952a22c594ca0fd53a94
Yigit Findik <yigit8@live.nl>
2023-12-01 12:18:48 +0100

    removed code that codecommented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,11 @@
 import React, { useState } from "react";
 import LandingPage from "./LandingPage";
-// import ReactAudioPlayer from 'react-audio-player';
 
 
 const Home = () => {
-    // const [isMuted, setIsMuted] = useState(false);
-
-    // const handleToggleMute = () => {
-    //     setIsMuted(!isMuted);
-    // };
 
     return <div>
         <LandingPage />
-        {/*<ReactAudioPlayer*/}
-        {/*    src="/sounds/PokémonThemeSong.mp3"*/}
-        {/*    autoPlay*/}
-        {/*    volume={isMuted ? 0 : 1} // Set volume to 0 when muted, 1 otherwise*/}
-        {/*/>*/}
-        {/*<button onClick={handleToggleMute}>*/}
-        {/*    {isMuted ? "Unmute" : "Mute"}*/}
-        {/*</button>*/}
     </div>
 };
   
