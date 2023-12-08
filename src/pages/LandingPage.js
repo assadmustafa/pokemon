5fcd6004812fdad5ea9419133552309cc17dd149
valentijnoa <74962322+valentijnoa@users.noreply.github.com>
2023-12-08 09:21:25 +0100

    play button

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,45 +2,45 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
-    const navigate = useNavigate();
-    const [isMuted, setIsMuted] = useState(false);
+  const navigate = useNavigate();
+  const [isMuted, setIsMuted] = useState(false);
 
-    const onStart = () => {
-        // Replace '/game' with the actual path you want to navigate to
-        navigate("/game");
-    };
+  const onStart = () => {
+    // Replace '/game' with the actual path you want to navigate to
+    navigate("/game");
+  };
 
-    const handleToggleMute = () => {
-        setIsMuted(!isMuted);
-    };
+  const handleToggleMute = () => {
+    setIsMuted(!isMuted);
+  };
 
-    return (
-        <div className="container-landing">
-            {/* Local Video */}
-            <video autoPlay loop muted={isMuted} className="video">
-                <source src="/videos/myvideo.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+  return (
+    <div className="container-landing">
+      {/* Local Video */}
+      <video autoPlay loop muted={isMuted} className="video">
+        <source src="/videos/myvideo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-            {/* Content in the middle */}
-            <div className="content">
-                {/* Animated Gradient Text */}
-                <h1>
-                    <img src="./images/hero.png" alt="Hero" />
-                </h1>
+      {/* Content in the middle */}
+      <div className="content">
+        {/* Animated Gradient Text */}
+        <h1>
+          <img src="./images/hero.png" alt="Hero" />
+        </h1>
 
-                {/* Pokeball-style Start Button */}
-                <button onClick={onStart} className="pokeball">
-                    <img src="/images/pokeball.png" alt="Start" />
-                </button>
+        {/* Pokeball-style Start Button */}
+        <button onClick={onStart} className="pokeball">
+          Start Game
+        </button>
 
-                {/* Mute Button in Bottom Left */}
-                <button className="mute-button" onClick={handleToggleMute}>
-                    {isMuted ? "Unmute" : "Mute"}
-                </button>
-            </div>
-        </div>
-    );
+        {/* Mute Button in Bottom Left */}
+        <button className="mute-button" onClick={handleToggleMute}>
+          {isMuted ? "Unmute" : "Mute"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
